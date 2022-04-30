1a93204af01c7dca73d3afb0669f1e79c6cb1932
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-01 06:03:07 +0700

    feat(messsages/postgre-errors): create constants for postgre error

---
+++ utils/constants/messages/postgre-errors.js
@@ -0,0 +1,8 @@
+/**
+ * @description list of all errors https://www.postgresql.org/docs/current/errcodes-appendix.html
+ */
+const postgreErrors = {
+  UNIQUE_VIOLATION: "23505",
+};
+
+export default postgreErrors;

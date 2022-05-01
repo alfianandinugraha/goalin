9d54fda89e5ca67dc383bfd9d9e5c32fc0a092db
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-02 00:38:06 +0700

    feat(FE): create GoalItem fragment and custom primary color

--- frontend/app/src/main/java/com/example/goalin/MainActivity.kt
+++ frontend/app/src/main/java/com/example/goalin/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.goalin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.goalin.fragments.GoalItemFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment, GoalItemFragment())
+            .commit()
     }
 }

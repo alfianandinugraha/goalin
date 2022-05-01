1c8dde9dc6a01ddaf4617489b3f7c6c7fa505141
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-01 16:11:06 +0700

    feat(helpers/connect): allow cors

--- utils/helpers/connect.js
+++ utils/helpers/connect.js
@@ -1,19 +1,20 @@
 import nc, { NextConnect } from "next-connect";
 import { NextApiRequest, NextApiResponse } from "next";
 import tokenMiddleware from "utils/middlewares/token";
+import cors from "cors";
 
 /**
  * @returns {NextConnect<NextApiRequest, NextApiResponse>}
  */
 const connect = () => {
-  return nc();
+  return nc().use(cors());
 };
 
 /**
  * @returns {NextConnect<NextApiRequest, NextApiResponse>}
  */
 const connectAuth = () => {
-  return nc().use(tokenMiddleware);
+  return nc().use(cors()).use(tokenMiddleware);
 };
 
 export { connectAuth };

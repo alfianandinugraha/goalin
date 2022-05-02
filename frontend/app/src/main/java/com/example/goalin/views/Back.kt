ec83299684570a4d2fd5a5f443cf751076b062cc
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-02 22:56:45 +0700

    feat(FE:activity_add_goal.xml): create layout for add goal

---
+++ frontend/app/src/main/java/com/example/goalin/views/Back.kt
@@ -0,0 +1,20 @@
+package com.example.goalin.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.example.goalin.R
+
+class Back(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    init {
+        LayoutInflater
+            .from(context)
+            .inflate(R.layout.view_back,this)
+
+        setOnClickListener {
+            Log.d("Event", "Back...")
+        }
+    }
+}

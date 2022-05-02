415448960b3ed1908458268f660956717747f62c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-03 03:41:26 +0700

    refactor(FE:views/Button): use style config

--- frontend/app/src/main/java/com/example/goalin/views/Button.kt
+++ frontend/app/src/main/java/com/example/goalin/views/Button.kt
@@ -3,16 +3,11 @@ package com.example.goalin.views
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.CompoundButton
-import androidx.core.content.ContextCompat
+import androidx.appcompat.view.ContextThemeWrapper
 import com.example.goalin.R
 
-class Button(context: Context, attrs: AttributeSet): CompoundButton(context, attrs) {
+class Button(context: Context, attrs: AttributeSet): CompoundButton(ContextThemeWrapper(context, R.style.style_button), attrs, R.style.style_button) {
     init {
-        textAlignment = TEXT_ALIGNMENT_CENTER
-        setTextAppearance(R.style.button_text)
-
-        background = ContextCompat.getDrawable(context, R.drawable.button_state)
-
         setOnClickListener {
             isPressed = true
         }

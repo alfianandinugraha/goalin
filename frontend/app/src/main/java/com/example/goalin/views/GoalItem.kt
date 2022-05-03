2409bda8264718b4709978b92d08efa9e6c9384c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-03 08:16:04 +0700

    feat(FE:activity_goal.xml): create activity_goal.xml without list of transaction

--- frontend/app/src/main/java/com/example/goalin/views/GoalItem.kt
+++ frontend/app/src/main/java/com/example/goalin/views/GoalItem.kt
@@ -1,9 +1,11 @@
 package com.example.goalin.views
 
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import com.example.goalin.GoalActivity
 import com.example.goalin.R
 
 class GoalItem(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
@@ -11,5 +13,11 @@ class GoalItem(context: Context, attrs: AttributeSet) : LinearLayout(context, at
         LayoutInflater
             .from(context)
             .inflate(R.layout.view_goal_item, this)
+
+        val goalActivity = Intent(context, GoalActivity::class.java)
+
+        setOnClickListener {
+            context.startActivity(goalActivity)
+        }
     }
 }

f45cfee41a6ce7c9140b371d0bdca15652111268
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-03 11:11:27 +0700

    feat(FE:views/BackView): add back based on class

--- frontend/app/src/main/java/com/example/goalin/views/BackView.kt
+++ frontend/app/src/main/java/com/example/goalin/views/BackView.kt
@@ -2,8 +2,8 @@ package com.example.goalin.views
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.example.goalin.R
@@ -14,10 +14,20 @@ class BackView(context: Context, attrs: AttributeSet) : LinearLayout(context, at
             .from(context)
             .inflate(R.layout.view_back,this)
 
-        setOnClickListener {
-            Log.d("Event", "Back...")
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.BackView)
+        val backActivity = typedArray.getString(R.styleable.BackView_backTo)
+
+        val backButton = findViewById<LinearLayout>(R.id.back_btn)
+        backButton.setOnClickListener {
             val activity: Activity = context as Activity
-            activity.finish()
+            if (backActivity == null) {
+                activity.finish()
+                return@setOnClickListener
+            }
+
+            val className = Class.forName("com.example.goalin$backActivity")
+            val backIntent = Intent(context, className)
+            activity.startActivity(backIntent)
         }
     }
 }

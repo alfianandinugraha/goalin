b20dea561ce44bfbf5a5c33446ea182fdb4acb61
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-08 14:26:04 +0700

    fix(FE:BackView): override click listener

--- frontend/app/src/main/java/com/example/goalin/ui/BackView.kt
+++ frontend/app/src/main/java/com/example/goalin/ui/BackView.kt
@@ -4,19 +4,34 @@ import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import com.example.goalin.R
 
 class BackView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    private var view: LinearLayout;
+    private val baseContext = context;
+
     init {
         LayoutInflater
             .from(context)
             .inflate(R.layout.view_back,this)
 
-        val backButton = findViewById<LinearLayout>(R.id.back_btn)
-        backButton.setOnClickListener {
-            val activity: Activity = context as Activity
-            activity.finish()
+        view = findViewById(R.id.back_btn)
+
+        setOnClickListener {
+            back()
+        }
+    }
+
+    fun back() {
+        val activity: Activity = baseContext as Activity
+        activity.finish()
+    }
+
+    fun setOnClickListener(listener: (view: View) -> Unit) {
+        view.setOnClickListener {
+            listener(this)
         }
     }
 }

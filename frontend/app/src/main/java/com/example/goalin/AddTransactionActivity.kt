94a5f6f7701be47f4d18006c0cc823697495b516
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-08 11:51:45 +0700

    feat(FE:AddTransactionActivity): fetch wallet

--- frontend/app/src/main/java/com/example/goalin/AddTransactionActivity.kt
+++ frontend/app/src/main/java/com/example/goalin/AddTransactionActivity.kt
@@ -2,14 +2,45 @@ package com.example.goalin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.goalin.service.WalletService
+import com.example.goalin.util.http.ApiResponseException
 import com.example.goalin.views.ButtonView
+import com.example.goalin.views.SelectView
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AddTransactionActivity : AppCompatActivity() {
+    private val scope = CoroutineScope(CoroutineName("AddTransactionScope") + Dispatchers.IO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_transaction)
 
         val saveButton = findViewById<ButtonView>(R.id.save_btn)
+        val walletSelectView = findViewById<SelectView<String>>(R.id.wallet_select_view)
+
+        val walletService = WalletService(this)
+
+        scope.launch {
+            try {
+                val response = walletService.getAll()
+                withContext(Dispatchers.Main) {
+                    walletSelectView.options = response.payload.map {
+                        SelectView.Option(it.name, it.id)
+                    }
+                }
+            } catch (err: ApiResponseException) {
+                withContext(Dispatchers.Main) {
+                    Toast
+                        .makeText(this@AddTransactionActivity, err.response().message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
 
         saveButton.setOnClickListener {
             finish()

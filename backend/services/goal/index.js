bbe1ae8c354452630b99eacbdc5d4093485c5255
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-09 01:05:57 +0700

    fix(BE:goalServices): order desc by created_at on getAllGoals

--- backend/services/goal/index.js
+++ backend/services/goal/index.js
@@ -79,7 +79,8 @@ const getAllGoals = async (userId) => {
         )
       `
     )
-    .eq("user_id", userId);
+    .eq("user_id", userId)
+    .order("created_at", { ascending: false });
 
   if (error) {
     console.log(error);

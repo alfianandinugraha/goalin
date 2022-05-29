49be2562687848d8ee42bb7757454ef04c74da9b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-29 22:31:47 +0700

    feat(BE:TransactionServices): add fetch all transaction

--- backend/pages/api/goals/[goalId]/transactions/index.js
+++ backend/pages/api/goals/[goalId]/transactions/index.js
@@ -1,4 +1,5 @@
 import Ajv from "ajv";
+import goalServices from "services/goal";
 import transactionServices from "services/transaction";
 import response from "utils/constants/messages/response";
 import { transactionBodySchema } from "utils/constants/schemas/transaction";
@@ -6,28 +7,58 @@ import { connectAuth } from "utils/helpers/connect";
 
 const ajv = new Ajv();
 
-export default connectAuth().post(async (req, res) => {
-  const validate = ajv.compile(transactionBodySchema);
+export default connectAuth()
+  .get(async (req, res) => {
+    const goalId = req.query.goalId;
 
-  if (!validate(req.body)) {
-    return res.status(400).json(response.INVALID_BODY);
-  }
+    try {
+      await goalServices.get({ goalId, userId: req.user.id });
+      const transactions = await transactionServices.getAll(goalId);
 
-  const goalId = req.query.goalId;
-  try {
-    const transaction = await transactionServices.create({
-      goalId,
-      ...req.body,
-    });
+      return res.json({
+        message: "Berhasil mendapatkan list transactions",
+        payload: transactions.map((item) => {
+          return {
+            id: item.transaction_id,
+            amount: item.amount,
+            createdAt: item.created_at,
+            goalId: item.goal_id,
+            wallet: {
+              id: item.wallet.wallet_id,
+              name: item.wallet.name,
+            },
+          };
+        }),
+      });
+    } catch (err) {
+      return res.status(400).json({
+        message: err.message,
+        payload: {},
+      });
+    }
+  })
+  .post(async (req, res) => {
+    const validate = ajv.compile(transactionBodySchema);
 
-    return res.json({
-      message: "Berhasil menyimpan transaction",
-      payload: transaction,
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: err.message,
-      payload: {},
-    });
-  }
-});
+    if (!validate(req.body)) {
+      return res.status(400).json(response.INVALID_BODY);
+    }
+
+    const goalId = req.query.goalId;
+    try {
+      const transaction = await transactionServices.create({
+        goalId,
+        ...req.body,
+      });
+
+      return res.json({
+        message: "Berhasil menyimpan transaction",
+        payload: transaction,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        message: err.message,
+        payload: {},
+      });
+    }
+  });
